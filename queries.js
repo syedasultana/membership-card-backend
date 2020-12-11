d4d2295ac680fd8e62c36acaaacf445b3fd33fe3
syeda sultana <syedamahasultana@gmail.com>
2020-12-11 10:28:50 +0000

    set up working endpoint with postgres

---
+++ queries.js
@@ -0,0 +1,21 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'syedasultana',
+  host: 'localhost',
+  database: 'membership',
+  port: 5432,
+})
+
+const getProducts = async (request, response) => {
+    await pool.query('SELECT * FROM products', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+}
+
+module.exports = {
+    getProducts
+    
+}
